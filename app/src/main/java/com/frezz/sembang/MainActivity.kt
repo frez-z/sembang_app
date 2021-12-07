f58262430708cc3482e95255c379e16b9402cad3
frez <farezamirul20@gmail.com>
2021-12-07 23:00:10 +0800

    modify

--- app/src/main/java/com/frezz/sembang/MainActivity.kt
+++ app/src/main/java/com/frezz/sembang/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.frezz.sembang
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import com.frezz.sembang.databinding.ActivityMainBinding
@@ -10,18 +11,19 @@ class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val intent = Intent(this, ShowActivity::class.java)
+        startActivity(intent)
+        this.finish()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         window.setBackgroundDrawableResource(R.drawable.background_default);
         setContentView(binding.root)
 
+
         binding.sendButton.setOnClickListener {
             Toast.makeText(this,binding.sendText.text.toString(), Toast.LENGTH_SHORT).show()
             binding.sendText.setText("")
         }
-//        val intent = Intent(this, ShowActivity::class.java)
-//        startActivity(intent)
-//        this.finish()
     }
 
     override fun onStart() {
