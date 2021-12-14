7ad3809e9567a1343f3891821d511c8a749c4686
frez <farezamirul20@gmail.com>
2021-12-14 20:10:33 +0800

    update and create new activity

--- app/src/main/java/com/frezz/sembang/MainActivity.kt
+++ app/src/main/java/com/frezz/sembang/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.frezz.sembang
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import com.frezz.sembang.databinding.ActivityMainBinding
+
+
+
 
 class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val intent = Intent(this, ShowActivity::class.java)
-        startActivity(intent)
-        this.finish()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        window.setBackgroundDrawableResource(R.drawable.background_default);
+        window.setBackgroundDrawableResource(R.drawable.background_default)
         setContentView(binding.root)
 
 
